f552f24f9123a32f54872469c6ab84493adcd75c
Sara <sara.m.adamski@gmail.com>
2021-11-05 18:14:04 -0500

    list and play songs

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import './App.css';
-import Amplify, { API, graphqlOperation } from 'aws-amplify';
+import Amplify from 'aws-amplify';
 import awsconfig from './aws-exports';
 import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
-import { listSongs } from './graphql/queries';
+import SongList from "./components/SongList";
 
 Amplify.configure(awsconfig);
 
 function App() {
-  const [songs, setSongs] = useState([]);
-
-  const fetchSongs = async () => {
-    try {
-      const songData = await API.graphql(graphqlOperation(listSongs));
-      const songList = songData.data.listSongs.items;
-      console.log("Song list: ", songList);
-      setSongs(songList);
-    } catch (error) {
-      console.log("Error on fetching songs: ", error)
-    }
-  }
-
-  useEffect(() => {
-    fetchSongs();
-  }, []);
-
-
   return (
     <div className="App">
       <header className="App-header">
+        <h1>My Songs</h1>
         <AmplifySignOut />
-        <h2>My App Content</h2>
       </header>
+      <SongList />
     </div>
   );
 }
---
+++ src/components/SongList.js
@@ -0,0 +1,110 @@
+import React, { useState, useEffect } from "react";
+import Amplify, { API, graphqlOperation, Storage } from 'aws-amplify';
+import awsconfig from '../aws-exports';
+import { listSongs } from '../graphql/queries';
+import { updateSong } from '../graphql/mutations';
+import { Paper, IconButton } from '@material-ui/core';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import PauseIcon from '@material-ui/icons/Pause';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ReactPlayer from 'react-player';
+
+// HOW TO UPLOAD OUR SONGS
+
+Amplify.configure(awsconfig);
+
+const SongList = () => {
+  const [songs, setSongs] = useState([]);
+  const [songPlaying, setSongPlaying] = useState("");
+  const [audioURL, setAudioURL] = useState("");
+
+  const fetchSongs = async () => {
+    try {
+      const songData = await API.graphql(graphqlOperation(listSongs));
+      const songList = songData.data.listSongs.items;
+      console.log("Song list: ", songList);
+      setSongs(songList);
+    } catch (error) {
+      console.log("Error on fetching songs: ", error)
+    }
+  }
+
+  useEffect(() => {
+    fetchSongs();
+  }, []);
+
+  const toggleSong = async idx => {
+    if (songPlaying === idx) {
+      setSongPlaying('');
+      return;
+    }
+
+    const songFilePath = songs[idx].filePath;
+    try {
+      const fileAccessURL = await Storage.get(songFilePath, { expires: 60 });
+      console.log("access url: ", fileAccessURL);
+      setSongPlaying(idx);
+      setAudioURL(fileAccessURL);
+      return;
+    } catch (error) {
+      console.error('Error accessing file from S3: ', error);
+      setAudioURL("");
+      setSongPlaying("");
+    }
+  }
+
+  const addLike = async idx => {
+    try {
+      const song = songs[idx];
+      song.likes = song.likes + 1;
+      delete song.createdAt;
+      delete song.updatedAt;
+
+      const songData = await API.graphql(graphqlOperation(updateSong, { input: song }));
+      const songList = [...songs];
+      songList[idx] = songData.data.updateSong;
+      setSongs(songList);
+    } catch (error) {
+      console.log("addLike error: ", error)
+    }
+  }
+
+
+  return (
+    <div className="songList">
+      {songs.map((song, idx) => (
+        <Paper variant="outlined" elevation={2} key={`song${idx}`}>
+          <div className="songCard">
+            <IconButton aria-label="play" onClick={() => toggleSong(idx)}>
+              {songPlaying === idx ? <PauseIcon /> : <PlayArrowIcon />}
+            </IconButton>
+              <div>
+                <div className="songTitle">{song.title}</div>
+                <div className="songOwner">{song.owner}</div>
+              </div>
+              <div>
+                <IconButton aria-label="like" onClick={() => addLike(idx)}>
+                  <FavoriteIcon />
+                </IconButton>
+                {song.likes}
+              </div>
+              <div className="songDescription">{song.description}</div>
+          </div>
+          {songPlaying === idx ? (
+            <div className="ourAudioPlayer">
+              <ReactPlayer
+                url={audioURL}
+                controls
+                playing
+                height="50px"
+                onPause={() => toggleSong(idx)}
+              />
+            </div>
+          ) : null}
+        </Paper>
+      ))}
+    </div>
+  );
+}
+
+export default SongList;
